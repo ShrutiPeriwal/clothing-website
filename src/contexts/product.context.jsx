a8b2068a2ab7161c759698ab037ae5cbaba9d2fb
Shruti <shrutiperiwal21@gmail.com>
2022-10-10 22:40:43 +0530

    checkout

--- src/contexts/product.context.jsx
+++ src/contexts/product.context.jsx
@@ -1,12 +1,12 @@
 import { createContext, useState } from 'react';
-import PRODUCTS from '../shop.data.json';
+import SHOP_DATA from '../shop.data.js';
 
 export const ProductContext = createContext({
     products: [], 
 });
 
 export const ProductProvider = ({ children }) => {
-    const [products] = useState(PRODUCTS);
+    const [products] = useState([]);
     const value = { products };
     return (
         <ProductContext.Provider value={value}>{children}</ProductContext.Provider>
